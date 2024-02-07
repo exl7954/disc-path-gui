6b78d4020c8e34509314a24b2626fc91a7f2398c
exl7954 <exl7954@nyu.edu>
2024-02-06 20:23:50 -0500

    initial commit

---
+++ ros-system/build/pathserver/CMakeFiles/pathserver_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathserver_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pathserver_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
