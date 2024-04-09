30d41c72c9589d34102d487001c3e64942a6b78f
exl7954 <exl7954@nyu.edu>
2024-04-09 17:42:14 -0400

    fixed small polygon bug

--- react-gui/src/components/Polygon/index.jsx
+++ react-gui/src/components/Polygon/index.jsx
@@ -1,7 +1,23 @@
-import {useDrawPolygon} from "./useDrawPolygon";
+import { useCanvas } from "../CanvasProvider";
 
 const Polygon = (props) => {
-    useDrawPolygon(props);
+    const { draw } = useCanvas();
+
+    draw((ctx) => {
+        ctx.fillStyle = props.color;
+        ctx.beginPath();
+        props.points.forEach(([x, y], index) => {
+            if (index === 0) {
+                ctx.moveTo(x, y);
+            }
+            else {
+                ctx.lineTo(x, y);
+            }
+        });
+        ctx.closePath();
+        ctx.fill();
+    });
+
     return null;
 }
 
